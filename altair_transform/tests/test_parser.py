338897bf5a3203aab48393e124216d914c60113c
Jake VanderPlas <jakevdp@google.com>
2019-02-25 13:27:23 -0800

    TST: add parser tests for name expressions

--- altair_transform/tests/test_parser.py
+++ altair_transform/tests/test_parser.py
@@ -15,6 +15,14 @@ EXPRESSIONS = [
     "2 * 3 ** 4"
 ]
 
+EXPRESSIONS_WITH_NAMES = [
+    "A * 1",
+    "B + 2",
+    "3 * (4 + C)"
+]
+
+
+
 
 @pytest.fixture
 def parser():
@@ -24,3 +32,9 @@ def parser():
 @pytest.mark.parametrize('expression', EXPRESSIONS)
 def test_simple_eval(expression, parser):
     assert eval(expression) == parser.parse(expression)
+
+
+@pytest.mark.parametrize('expression', EXPRESSIONS_WITH_NAMES)
+def test_name_eval(expression, parser):
+    names = {'A': 5, 'B': 6, 'C': 7}
+    assert eval(expression, names) == parser.parse(expression, names)
