da87be52eabc448358075c751fe41d9b2c4cfb73
Jake VanderPlas <jakevdp@google.com>
2019-03-08 08:36:38 -0800

    Add aggregate transform

--- altair_transform/core.py
+++ altair_transform/core.py
@@ -5,6 +5,9 @@ def apply_transform(df, transforms, inplace=False):
     if not inplace:
         df = df.copy()
 
+    if isinstance(transforms, dict):
+        transforms = [transforms]
+
     for transform in transforms:
         transform = alt.Transform.from_dict(transform)
         if isinstance(transform, alt.CalculateTransform):
@@ -16,6 +19,19 @@ def apply_transform(df, transforms, inplace=False):
             func = lambda datum: eval_vegajs(transform.filter, datum)
             mask = df.apply(func, axis=1).astype(bool)
             df = df[mask]
+        elif isinstance(transform, alt.AggregateTransform):
+            groupby = transform['groupby']
+            for aggregate in transform['aggregate']:
+                op = aggregate['op'].to_dict()
+                field = aggregate['field']
+                col = aggregate['as']
+
+                if groupby is alt.Undefined:
+                    df[col] = df[field].aggregate(op)
+                else:
+                    result = df.groupby(groupby)[field].aggregate(op)
+                    result.name = col
+                    df = df.join(result, on=groupby)
         else:
             raise NotImplementedError(
                 f"{transform.__class__.__name__} not implemented.")
--- altair_transform/tests/test_core.py
+++ altair_transform/tests/test_core.py
@@ -11,12 +11,13 @@ def data():
     rand = np.random.RandomState(42)
     return pd.DataFrame({
         'x': rand.randint(0, 100, 10),
-        'y': rand.randint(0, 100, 10)
+        'y': rand.randint(0, 100, 10),
+        'c': list('AABBCCCDDD'),
     })
 
 
 def test_calculate_transform(data):
-    transform = [{'calculate': 'datum.x + datum.y', 'as': 'z'}]
+    transform = {'calculate': 'datum.x + datum.y', 'as': 'z'}
     out1 = apply_transform(data, transform)
 
     out2 = data.copy()
@@ -26,10 +27,29 @@ def test_calculate_transform(data):
 
 
 def test_filter_transform(data):
-    transform = [{'filter': 'datum.x < datum.y'}]
+    transform = {'filter': 'datum.x < datum.y'}
     out1 = apply_transform(data, transform)
 
     out2 = data.copy()
     out2 = out2[data.x < data.y]
 
     assert out1.equals(out2)
+
+
+@pytest.mark.parametrize('groupby', [True, False])
+def test_aggregate_transform(data, groupby):
+    op = 'sum'
+    field = 'x'
+    col = 'z'
+    group = 'c'
+
+    transform = {'aggregate': [{'op': op, 'field': field, 'as': col}]}
+    if groupby:
+        transform['groupby'] = [group]
+    
+    out = apply_transform(data, transform)
+    validate = lambda group: group[field].aggregate(op) == group[col]
+    if groupby:
+        assert out.groupby(group).apply(validate).all()
+    else:
+        assert validate(out).all()
