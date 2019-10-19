d57a5853db3b52bcf85c10892097b37b3eed9dce
Jake VanderPlas <jakevdp@google.com>
2019-10-18 20:35:00 -0700

    MAINT: test on Python 3.8

--- setup.py
+++ setup.py
@@ -75,5 +75,6 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
