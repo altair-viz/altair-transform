60f805bbd997ddcafc89e63090576b8887ed86e2
Jake VanderPlas <jakevdp@google.com>
2020-01-17 19:49:27 -0800

    TST: add quantile driver tests

--- altair_transform/transform/tests/test_quantile.py
+++ altair_transform/transform/tests/test_quantile.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict, List, Optional
+
 import numpy as np
 from numpy.testing import assert_allclose
 import pandas as pd
@@ -31,3 +33,35 @@ def test_quantile_transform_groupby(data: pd.DataFrame) -> None:
         out_group_1 = altair_transform.apply(data[data[group] == key], transform)
         out_group_2 = out[out[group] == key][out_group_1.columns].reset_index(drop=True)
         assert_frame_equal(out_group_1, out_group_2)
+
+
+@pytest.mark.parametrize("step", [None, 0.1])
+@pytest.mark.parametrize("groupby", [None, ["c"]])
+@pytest.mark.parametrize("probs", [None, [0.2 * i for i in range(6)]])
+@pytest.mark.parametrize("as_", [None, ["p", "q"]])
+def test_bin_against_js(
+    driver,
+    data: pd.DataFrame,
+    step: Optional[float],
+    groupby: Optional[List[str]],
+    probs: Optional[List[float]],
+    as_: Optional[List[str]],
+) -> None:
+    transform: Dict[str, Any] = {"quantile": "x"}
+    if step is not None:
+        transform["step"] = step
+    if groupby is not None:
+        transform["groupby"] = groupby
+    if probs is not None:
+        transform["probs"] = probs
+    if as_ is not None:
+        transform["as"] = as_
+    got = altair_transform.apply(data, transform)
+    want = driver.apply(data, transform)
+    assert_frame_equal(
+        got[sorted(got.columns)],
+        want[sorted(want.columns)],
+        check_dtype=False,
+        check_index_type=False,
+        check_less_precise=True,
+    )
